7f96b2340d759cb31b5d54796ea42c30f276644d
Rick Moynihan <rick.m@swirrl.com>
2013-12-12 16:57:09 +0000

    Remove hardcoded values for GetDocumentController so it works with passed values.

--- app/controllers/get_controller.rb
+++ app/controllers/get_controller.rb
@@ -2,7 +2,7 @@ class GetController < ApplicationController
   # Eg http://localhost:3000/openapi/eldis/get/documents/A64559/full
   def documents
     service = DocumentService.build
-    @document = service.get(type: "eldis", id: "A64559", detail: params[:detail])
+    @document = service.get(type: params[:graph], id: params[:id], detail: params[:detail])
 
     response.headers["Content-Type"] = "application/json"
     render json: @document
