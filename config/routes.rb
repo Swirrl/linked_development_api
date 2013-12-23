6ca7e83bee751bfa5badfc0ca277510cccb5e9b9
Rick Moynihan <rick.m@swirrl.com>
2013-12-23 15:37:40 +0000

    Redirect root to http://linked-development.org/ (PMD app)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 LinkedDevelopmentApi::Application.routes.draw do
+  root :to => redirect('http://linked-development.org/')
+
   scope '/openapi' do
     get '/:graph/get/documents/:id(/:detail)', to: 'get#documents'
     get '/:graph/get/themes/:id(/:detail)', to: 'get#themes'
